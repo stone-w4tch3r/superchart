ddb0987239706e68a4edfcf22ad7022f1997075c
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-06-13 01:57:37 +0500

    added examples pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
+import Dashboard from "./dashboard_example/Dashboard.tsx";
+import LandingPage from "./landing_example/LandingPage.tsx";
 
 const App: React.FC = () => {
     return <Router>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/landing" element={<LandingPage/>}/>
         </Routes>
     </Router>;
 };
