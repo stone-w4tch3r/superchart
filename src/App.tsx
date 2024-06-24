49ab1dfeb5e59ec272938f9fb34461b3fa931bf8
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-06-25 04:49:29 +0500

    HashRouter instead of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import ChartPage from './chartModule/ChartPage.tsx';
 import Dashboard from "./dashboard_example/Dashboard.tsx";
 import LandingPage from "./landing_example/LandingPage.tsx";
