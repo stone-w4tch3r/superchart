7215a646bf937647c27eb9a35e6a62748683764a
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-06-25 04:19:36 +0500

    testing issues with gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,7 @@ import Dashboard from "./dashboard_example/Dashboard.tsx";
 import LandingPage from "./landing_example/LandingPage.tsx";
 
 const App: React.FC = () => {
-    return <Router>
-        <Routes>
-            <Route path="/" element={<ChartPage/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/landing" element={<LandingPage/>}/>
-        </Routes>
-    </Router>;
+    return <h1>Hello World</h1>;
 };
 
 export default App;
