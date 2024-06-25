b8fcb233c99753915f4b797e50fb9335682ae63b
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-06-25 18:12:13 +0500

    added models and routes mappings

---
+++ server/SuperchartBackend/SuperchartBackend/Todo.cs
@@ -0,0 +1,8 @@
+namespace SuperchartBackend;
+
+public class Todo
+{
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
---
+++ server/SuperchartBackend/SuperchartBackend/TodoDb.cs
@@ -0,0 +1,12 @@
+namespace SuperchartBackend;
+
+using Microsoft.EntityFrameworkCore;
+
+internal class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
