285863c835f2f8042a5d2f0655741ced09b81299
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-06-26 05:51:25 +0500

    refactored and simplified components

--- web/src/chartModule/ChartPage.tsx
+++ web/src/chartModule/ChartPage.tsx
@@ -1,8 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import Layout from './components/Layout';
 import Chart from './components/Chart';
 import {useFetchRoute} from "./useFetchRoute.ts";
-import {Skeleton, Typography} from "@mui/material";
+import {AppBar, Skeleton, Toolbar, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import {IPoint, ITrack} from "./types.ts";
@@ -24,6 +22,36 @@ const ChartPage: React.FC = () => {
     </Layout>;
 };
 
+const Layout: React.FC<{ children: React.ReactNode }> = ({children}) => {
+    return <>
+        <AppBar position="static"
+            // sx={{
+            //     height: {xl: "10vh"}
+            // }}
+        >
+            <Toolbar sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: "flex-start",
+                justifyContent: "center",
+                // height: "100vh",
+            }}>
+                <Typography variant="h3">
+                    Route Visualization
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <Stack alignItems={"center"}
+               spacing={2}
+               paddingX={5}
+               paddingTop={2}
+               height={'100vh'}
+        >
+            {children}
+        </Stack>
+    </>;
+}
+
 function renderChartBoxContent(points: IPoint[], tracks: ITrack[], isLoading: boolean, error: React.ReactNode) {
     if (isLoading) {
         return <Skeleton variant="rectangular" animation='wave' width="100%" height="100px"/>
@@ -31,7 +59,7 @@ function renderChartBoxContent(points: IPoint[], tracks: ITrack[], isLoading: bo
     if (error) {
         return <Typography variant="h3" color="error">{error}</Typography>
     }
-    return <Chart data={{points, tracks}}/>
+    return <Chart points={points} tracks={tracks}/>;
 }
 
 export default ChartPage;
--- web/src/chartModule/components/Chart.tsx
+++ web/src/chartModule/components/Chart.tsx
@@ -9,7 +9,8 @@ import {
     ChartsXAxis,
     ChartsYAxis,
     LinePlot,
-    MarkPlot, useDrawingArea
+    MarkPlot,
+    useDrawingArea
 } from "@mui/x-charts";
 import Paper from "@mui/material/Paper";
 import {Typography} from "@mui/material";
@@ -21,141 +22,108 @@ interface IChartPoint {
     distanceFromStart: number;
 }
 
-interface ChartProps {
-    data: { points: IPoint[], tracks: ITrack[] };
-}
-
-const Chart: React.FC<ChartProps> = ({data}) => {
-    if (data.points.length === 0 || data.tracks.length === 0) {
+const Chart: React.FC<{ points: IPoint[], tracks: ITrack[] }> = ({points, tracks}) => {
+    if (!points.length || !tracks.length) {
         return <Paper sx={{padding: 3, backgroundColor: "pink"}}>
             <Typography variant="caption">No data to display</Typography>
         </Paper>;
     }
-    
-    const {
-        pointSequences,
-        surfacesWithXCoordinates,
-        xValues,
-        chartPoints
-    } = convertToChartData(data.points, data.tracks);
 
+    const {pointSequences, surfacesWithXCoordinates, xValues, chartPoints} = convertToChartData(points, tracks);
     const coloredColumns = surfacesWithXCoordinates.map(
         ([x1, x2, surface]) => [x1, x2, surfaceToColor[surface]] as [number, number, string]
     );
-    const series = pointSequences.map(
-        ([speed, points]) => ({
-            data: points.map(point => point?.height),
-            type: 'line',
-            color: speedToColor[speed],
-            curve: "linear",
-        } as AllSeriesType)
-    );
+    const series = pointSequences.map(([speed, points]) => ({
+        data: points.map(point => point?.height),
+        type: 'line',
+        color: speedToColor[speed],
+        curve: "linear",
+    } as AllSeriesType));
 
-    return <ChartContainer
-        series={series}
-        xAxis={[{
-            scaleType: 'linear',
-            data: xValues,
-            max: xValues[xValues.length - 1],
-            id: 'x-linear-axis-id',
-        }]}
-        width={800}
-        height={400}
-    >
+    return <ChartContainer series={series}
+                           xAxis={[{scaleType: 'linear', data: xValues, max: Math.max(...xValues), id: 'x-axis'}]}
+                           width={800} height={400}>
         <Background coloredColumns={coloredColumns}/>
         <LinePlot/>
         <MarkPlot slotProps={{mark: {color: "black"}}}/>
-        <ChartsXAxis label="X axis" position="bottom" axisId="x-linear-axis-id"/>
+        <ChartsXAxis label="X axis" position="bottom" axisId="x-axis"/>
         <ChartsYAxis label="Y axis" position="left"/>
         <ChartsAxisHighlight x="line"/>
         <ChartsTooltip trigger="axis" slots={{axisContent: createTooltip(chartPoints)}}/>
-    </ChartContainer>
+    </ChartContainer>;
 };
 
 export default Chart;
 
-function createTooltip(points: IChartPoint[]): React.FC<ChartsAxisContentProps> {
-    return (props) => {
-        const {dataIndex} = props;
-        const point = points[dataIndex];
-        return (
-            <Paper sx={{padding: 3, backgroundColor: "pink"}}>
-                <Typography variant="body2">{point.name}</Typography>
-                <br/>
-                <Typography variant="caption">Height: {point.height}</Typography>
-                <br/>
-                <Typography variant="caption">Distance from start: {point.distanceFromStart}</Typography>
-            </Paper>
-        );
-    };
-}
+const createTooltip = (points: IChartPoint[]): React.FC<ChartsAxisContentProps> => ({dataIndex}) => {
+    const point = points[dataIndex];
+    return <Paper sx={{padding: 3, backgroundColor: "pink"}}>
+        <Typography variant="body2">{point.name}</Typography>
+        <Typography variant="caption">Height: {point.height}</Typography>
+        <br/>
+        <Typography variant="caption">Distance from start: {point.distanceFromStart}</Typography>
+    </Paper>;
+};
 
-const Background: React.FC<{ coloredColumns: [x1: number, x2: number, color: string][] }> = (p) => {
+const Background: React.FC<{ coloredColumns: [number, number, string][] }> = ({coloredColumns}) => {
     const {left, top, width, height} = useDrawingArea();
 
     return <>
-        {p.coloredColumns.map(([x1, x2, color], index) => {
+        {coloredColumns.map(([x1, x2, color], index) => {
             const columnLeft = left + (x1 / 100) * width;
             const columnWidth = ((x2 - x1) / 100) * width;
 
-            return (
-                <rect
-                    key={index}
-                    x={columnLeft}
-                    y={top}
-                    width={columnWidth}
-                    height={height}
-                    fill={color}
-                />
-            );
+            return <rect key={index} x={columnLeft} y={top} width={columnWidth} height={height} fill={color} style={{
+                animation: `drawColumnFromBottom 0.1s ease-out ${index * 0.1}s both`,
+                transformOrigin: 'bottom'
+            }}/>;
         })}
+        <style>
+            {`
+                @keyframes drawColumnFromBottom {
+                    from { transform: scaleY(0); }
+                    to { transform: scaleY(1); }
+                }
+            `}
+        </style>
     </>;
-}
+};
 
 const surfaceToColor: { [key in Surface]: string } = {
     [Surface.SAND]: 'brown',
     [Surface.ASPHALT]: 'pink',
     [Surface.GROUND]: 'gray',
-}
+};
 
 const speedToColor: { [key in MaxSpeed]: string } = {
     [MaxSpeed.FAST]: 'red',
     [MaxSpeed.NORMAL]: 'yellow',
     [MaxSpeed.SLOW]: 'green',
-}
+};
 
-function convertToChartData(points: IPoint[], tracks: ITrack[]): {
+const convertToChartData = (points: IPoint[], tracks: ITrack[]): {
     pointSequences: [MaxSpeed, (IChartPoint | null)[]][],
-    surfacesWithXCoordinates: [x1: number, x2: number, surface: Surface][],
+    surfacesWithXCoordinates: [number, number, Surface][],
     xValues: number[],
     chartPoints: IChartPoint[],
-} {
-    if (points.length === 0 || tracks.length === 0) {
-        return {pointSequences: [], surfacesWithXCoordinates: [], xValues: [], chartPoints: []};
-    }
+} => {
+    const chartPoints = calculatePointDistance(points, tracks).map(([point, distance]) => ({
+        id: point.id,
+        name: point.name,
+        height: point.height,
+        distanceFromStart: distance,
+    })).sort((a, b) => a.distanceFromStart - b.distanceFromStart);
 
-    const chartPoints: IChartPoint[] = calculatePointDistance(points, tracks)
-        .sort((a, b) => a[1] - b[1])
-        .map(([point, distance]) => ({
-            id: point.id,
-            name: point.name,
-            height: point.height,
-            distanceFromStart: distance,
-        }));
-
-    const pointSequences: [MaxSpeed, (IChartPoint | null)[]][] = createPointsSequences(chartPoints, tracks);
+    const pointSequences = createPointsSequences(chartPoints, tracks);
     const surfacesWithXCoordinates = getSurfacesWithXCoordinates(chartPoints, tracks);
     const xValues = chartPoints.map(point => point.distanceFromStart);
 
     return {pointSequences, surfacesWithXCoordinates, xValues, chartPoints};
-}
+};
 
-function createPointsSequences(points: IChartPoint[], tracks: ITrack[]): [MaxSpeed, (IChartPoint | null)[]][] {
+const createPointsSequences = (points: IChartPoint[], tracks: ITrack[]): [MaxSpeed, (IChartPoint | null)[]][] => {
     const sequences: [MaxSpeed, (IChartPoint | null)[]][] = [];
-    const idToPointMap: { [key: number]: IChartPoint } = {};
-    points.forEach(point => {
-        idToPointMap[point.id] = point;
-    });
+    const idToPointMap = Object.fromEntries(points.map(point => [point.id, point]));
 
     tracks.forEach(track => {
         const firstPoint = idToPointMap[track.firstId];
@@ -165,7 +133,7 @@ function createPointsSequences(points: IChartPoint[], tracks: ITrack[]): [MaxSpe
             throw new Error(`First or second point not found for track [${JSON.stringify(track)}]`);
         }
 
-        if (sequences.length === 0 || sequences[sequences.length - 1][0] !== track.maxSpeed) {
+        if (!sequences.length || sequences[sequences.length - 1][0] !== track.maxSpeed) {
             sequences.push([track.maxSpeed, new Array(points.length).fill(null)]);
         }
 
@@ -174,17 +142,13 @@ function createPointsSequences(points: IChartPoint[], tracks: ITrack[]): [MaxSpe
     });
 
     return sequences;
-}
+};
 
-function getSurfacesWithXCoordinates(points: IChartPoint[], tracks: ITrack[]): [x1: number, x2: number, surface: Surface][] {
-    const surfaces: [number, number, Surface][] = [];
+const getSurfacesWithXCoordinates = (points: IChartPoint[], tracks: ITrack[]): [number, number, Surface][] => {
+    const idToPointMap = Object.fromEntries(points.map(point => [point.id, point]));
     const maxDistance = points[points.length - 1].distanceFromStart;
-    const idToPointMap: { [key: number]: IChartPoint } = {};
-    points.forEach(point => {
-        idToPointMap[point.id] = point;
-    });
 
-    tracks.forEach(track => {
+    return tracks.map(track => {
         const firstPoint = idToPointMap[track.firstId];
         const secondPoint = idToPointMap[track.secondId];
 
@@ -192,25 +156,20 @@ function getSurfacesWithXCoordinates(points: IChartPoint[], tracks: ITrack[]): [
             throw new Error(`First or second point not found for track [${JSON.stringify(track)}]`);
         }
 
-        surfaces.push([
-            firstPoint.distanceFromStart / maxDistance * 100,
-            secondPoint.distanceFromStart / maxDistance * 100,
+        return [
+            (firstPoint.distanceFromStart / maxDistance) * 100,
+            (secondPoint.distanceFromStart / maxDistance) * 100,
             track.surface,
-        ]);
+        ];
     });
+};
 
-    return surfaces;
-}
-
-function calculatePointDistance(points: IPoint[], tracksOrdered: ITrack[]): [IPoint, number][] {
+const calculatePointDistance = (points: IPoint[], tracksOrdered: ITrack[]): [IPoint, number][] => {
     const pointToDistanceMap: { [key: number]: number } = {};
+    const idToPointsMap = Object.fromEntries(points.map(point => [point.id, point]));
+
     points.forEach(point => {
         pointToDistanceMap[point.id] = 0;
-    });
-
-    const idToPointsMap: { [key: number]: IPoint } = {};
-    points.forEach(point => {
-        idToPointsMap[point.id] = point;
     });
 
     tracksOrdered.forEach(track => {
@@ -225,4 +184,4 @@ function calculatePointDistance(points: IPoint[], tracksOrdered: ITrack[]): [IPo
     });
 
     return points.map(point => [point, pointToDistanceMap[point.id]]);
-}
+};
